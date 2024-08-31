a533f53e823812e551e3aef580bc9adfaa69c6ae
Jose Miguel Aguilar Torrealba <66124263+Migue1804@users.noreply.github.com>
2024-08-31 13:49:51 -0500

    Update pyvis_network_app.py

--- pyvis_network_app.py
+++ pyvis_network_app.py
@@ -5,103 +5,94 @@ import networkx as nx
 from pyvis.network import Network
 from PIL import Image
 
-# Verificar si la clave 'DATABASE_PASSWORD' existe en st.secrets
-if "DATABASE_PASSWORD" not in st.secrets:
-    st.error("No se encontró 'DATABASE_PASSWORD' en los secretos. Verifica la configuración en GitHub.")
+# Read dataset (CSV)
+df_survey = pd.read_csv('data/survey_results.csv')
+
+# Display the image above the title
+st.image('People.jpg', use_column_width=True)
+
+
+# Set header title
+st.title('Network Graph Visualization of People and Their Capacities')
+
+# Define list of unique capacities and sort alphabetically
+capacity_list = sorted(df_survey['capacity'].unique())
+
+# Define color mapping for capacities
+color_map = {
+    'Liderazgo': 'red',
+    'Comunicación': 'blue',
+    'Trabajo en equipo': 'green',
+    'Facilitación': 'orange',
+    'Gestion del cambio': 'purple'
+}
+
+# Implement multiselect dropdown menu for option selection (returns a list)
+selected_capacities = st.multiselect('Select capacity/capacities to visualize', capacity_list)
+
+# Set info message on initial site load
+if len(selected_capacities) == 0:
+    st.text('Choose at least 1 capacity to start')
+
+# Create network graph when user selects >= 1 item
 else:
-    # Pedir al usuario que ingrese la contraseña
-    password = st.text_input("Enter your password", type="password")
+    df_select = df_survey[df_survey['capacity'].isin(selected_capacities)]
+    df_select = df_select.reset_index(drop=True)
 
-    # Verificar si la contraseña ingresada coincide con la almacenada en los secretos
-    if password == st.secrets["DATABASE_PASSWORD"]:
-        st.success("Contraseña correcta!")
+    # Create networkx graph object from pandas dataframe
+    G = nx.from_pandas_edgelist(df_select, 'person_name', 'capacity', 'weight', create_using=nx.Graph())
 
-        # Mostrar la imagen
-        st.image('People.jpg', use_column_width=True)
+    # Initiate PyVis network object
+    person_net = Network(
+                         height='400px',
+                         width='100%',
+                         bgcolor='#222222',
+                         font_color='white'
+                        )
 
-        # Leer el dataset (CSV)
-        df_survey = pd.read_csv('data/survey_results.csv')
-
-        # Título de la aplicación
-        st.title('Análisis de redes organizacionales - Capacidades de las personas')
-
-        # Lista de capacidades únicas y ordenarlas alfabéticamente
-        capacity_list = sorted(df_survey['capacity'].unique())
-
-        # Mapeo de colores para las capacidades
-        color_map = {
-            'Liderazgo': 'red',
-            'Comunicación': 'blue',
-            'Trabajo en equipo': 'green',
-            'Facilitación': 'orange',
-            'Gestion del cambio': 'purple'
-        }
-
-        # Menú desplegable para seleccionar capacidades
-        selected_capacities = st.multiselect('Selecciona las capacidades a visualizar', capacity_list)
-
-        # Mensaje inicial si no se selecciona ninguna capacidad
-        if len(selected_capacities) == 0:
-            st.text('Escoge al menos una para iniciar')
+    # Add nodes with colors
+    for node in G.nodes():
+        if node in df_select['person_name'].values:
+            # Color nodes by person name default color (white)
+            person_net.add_node(node, label=node, color='white')
         else:
-            # Filtrar el DataFrame según las capacidades seleccionadas
-            df_select = df_survey[df_survey['capacity'].isin(selected_capacities)]
-            df_select = df_select.reset_index(drop=True)
+            # Color nodes by capacity
+            capacity = df_select[df_select['capacity'] == node]['capacity'].values[0]
+            person_net.add_node(node, label=node, color=color_map[capacity])
 
-            # Crear objeto de grafo con NetworkX a partir del DataFrame
-            G = nx.from_pandas_edgelist(df_select, 'person_name', 'capacity', 'weight', create_using=nx.Graph())
+    # Add edges with colors
+    for index, row in df_select.iterrows():
+        person_net.add_edge(row['person_name'], row['capacity'], color=color_map[row['capacity']], value=row['weight'])
 
-            # Iniciar objeto de PyVis Network
-            person_net = Network(
-                height='400px',
-                width='100%',
-                bgcolor='#222222',
-                font_color='white'
-            )
+    # Generate network with specific layout settings
+    person_net.repulsion(
+                         node_distance=420,
+                         central_gravity=0.33,
+                         spring_length=110,
+                         spring_strength=0.10,
+                         damping=0.95
+                        )
 
-            # Agregar nodos con colores
-            for node in G.nodes():
-                if node in df_select['person_name'].values:
-                    # Color de nodos por defecto (blanco)
-                    person_net.add_node(node, label=node, color='white')
-                else:
-                    # Color de nodos por capacidad
-                    capacity = df_select[df_select['capacity'] == node]['capacity'].values[0]
-                    person_net.add_node(node, label=node, color=color_map[capacity])
+    # Save and read graph as HTML file (on Streamlit Sharing)
+    try:
+        path = '/tmp'
+        person_net.save_graph(f'{path}/pyvis_graph.html')
+        HtmlFile = open(f'{path}/pyvis_graph.html', 'r', encoding='utf-8')
 
-            # Agregar bordes con colores
-            for index, row in df_select.iterrows():
-                person_net.add_edge(row['person_name'], row['capacity'], color=color_map[row['capacity']], value=row['weight'])
+    # Save and read graph as HTML file (locally)
+    except:
+        path = '/html_files'
+        person_net.save_graph(f'{path}/pyvis_graph.html')
+        HtmlFile = open(f'{path}/pyvis_graph.html', 'r', encoding='utf-8')
 
-            # Generar red con configuraciones específicas de diseño
-            person_net.repulsion(
-                node_distance=420,
-                central_gravity=0.33,
-                spring_length=110,
-                spring_strength=0.10,
-                damping=0.95
-            )
+    # Load HTML file in HTML component for display on Streamlit page
+    components.html(HtmlFile.read(), height=435)
 
-            # Guardar y leer el grafo como archivo HTML
-            try:
-                path = '/tmp'
-                person_net.save_graph(f'{path}/pyvis_graph.html')
-                HtmlFile = open(f'{path}/pyvis_graph.html', 'r', encoding='utf-8')
-            except:
-                path = '/html_files'
-                person_net.save_graph(f'{path}/pyvis_graph.html')
-                HtmlFile = open(f'{path}/pyvis_graph.html', 'r', encoding='utf-8')
-
-            # Cargar el archivo HTML en el componente HTML para mostrar en la página de Streamlit
-            components.html(HtmlFile.read(), height=435)
-
-        # Pie de página
-        st.markdown(
-            """
-            <br>
-            <h6>Para mayor información escribeme: https://www.linkedin.com/in/josemaguilar/ </h6>
-            """, unsafe_allow_html=True
-        )
-    else:
-        st.error("Contraseña incorrecta.")
+# Footer
+st.markdown(
+    """
+    <br>
+    <h6>Para mayor información escribeme: https://www.linkedin.com/in/josemaguilar/ </h6>
+    """, unsafe_allow_html=True
+)
 
